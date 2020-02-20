55120a0c5fedfc9c7e748a19c26db07870046f27
dumondmd <dmd.dumon@gmail.com>
2020-02-19 21:22:53 -0300

    Seller entity class

--- src/application/DemoDaoJdbc.java
+++ src/application/DemoDaoJdbc.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class DemoDaoJdbc {
 
     public static void main(String[] args) {
         Department obj = new Department(1, "Dumon");
-        System.out.println(obj);
+       
+        
+        Seller seller = new Seller(21, "Dumon2", "dumon@hotmail.com", new Date(), 1000.0, obj);
+        
+         System.out.println(seller);
     }
 
 }
